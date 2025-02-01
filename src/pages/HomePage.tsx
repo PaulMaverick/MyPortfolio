8f5c718a12de0dd05bc2917dc35da12b7679c7a3
Paul <paulpastor122297@gmail.com>
2025-02-01 11:21:33 +0800

    add social links

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,10 @@
 import styles from '../styles/homepage/hompage.module.css'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+
+
+
 
 export default function HomePage() {
     return (
@@ -7,6 +13,30 @@ export default function HomePage() {
                 <h5>Hello I'm a </h5>
                 <h1>Web Developer</h1>
                 <h2>I make softwares, websites for fun and professionaly</h2>
+
+                <div className={styles.buttons}>
+                    <a 
+                        href="https://github.com/PaulMaverick"
+                        target='_blank'
+                        rel='noopener noreferrer' 
+                    >
+                        <FaGithub />
+                    </a>
+                    <a 
+                        href="https://www.linkedin.com/in/paul-maverick-pastor-2b3b1b201/"
+                        target='_blank'
+                        rel='noopener noreferrer'                   
+                    >
+                        <FaLinkedin />
+                    </a>
+                    <a 
+                        href="https://drive.google.com/file/d/1aXbWvRxGYoU1BsRBA-Qn34pIrhc5UpTe/view?usp=sharing"
+                        target='_blank'
+                        rel='noopener noreferrer'                   
+                    >
+                        <FaFileAlt />
+                    </a>
+                </div>
             </article>
         </section>
     )
